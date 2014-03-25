59360db8b6bc6bfb514783ff1472161f6ddcecd0
Michael Stewart <michael@michael-stewart.com>
2014-03-26 00:13:58 +1100

    Added basic plotting of json data to map

--- app.py
+++ app.py
@@ -1,10 +1,43 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, abort
+
+UPLOAD_FOLDER = '/Users/michaelstewart/Documents/maps/uploads'
+DATA_FOLDER = '/Users/michaelstewart/Documents/maps/data'
+ALLOWED_EXTENSIONS = set(['txt', 'csv'])
+
 app = Flask(__name__, static_url_path='')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DATA_FOLDER'] = DATA_FOLDER
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
 
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return 'OK'
+        else:
+            abort(400)
+    else:
+        abort(400)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'])
+
+@app.route('/data/<filename>')
+def serve_data(filename):
+    return send_from_directory(app.config['DATA_FOLDER'], filename)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -9,3 +9,42 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+var markers = [];
+var line = [];
+
+var displayData = function(data) {
+	if (data == null)
+		return;
+	for (var i = 0; i < data.length; i++) {
+		var pos = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+		var marker = new google.maps.Marker({
+			position: pos,
+			map: map,
+			title: "Test",
+			animation: google.maps.Animation.DROP
+		});
+		markers.push(marker);
+		line.push(pos);
+		console.log(data[i]);
+	}
+	var path = new google.maps.Polyline({
+		path: line
+	});
+	path.setMap(map);
+};
+
+var init = function() {
+	$.ajax({
+		url: "/data/data.json",
+		success: displayData
+	});	
+}
+
+
+$(document).ready(function() {
+	$('#display').click(function(e) {
+		e.preventDefault();
+		init();
+	});
+});
